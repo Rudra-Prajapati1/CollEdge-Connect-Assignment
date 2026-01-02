10e9d8bda183e4f1e2fbe5495637f726f247b350
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2026-01-02 23:03:56 +0530

    Initial Commit

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/health", (req, res) => {
+  res.status(200).send("Server is running fine.");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
